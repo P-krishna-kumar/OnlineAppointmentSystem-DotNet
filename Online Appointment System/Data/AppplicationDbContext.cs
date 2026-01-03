8eeeb9371328725cf5e56b4f6f65a94213a690bc
P Krishna Kumar <kkrishna37768@gmail.com>
2026-01-03 12:31:34 +0530

    DbContext Added

---
+++ Online Appointment System/Data/AppplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Appointment_System.Models;
+using System.Data;
+
+namespace Online_Appointment_System.Data
+{
+    public class ApplicationDbContext : DbContext   
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+         
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
