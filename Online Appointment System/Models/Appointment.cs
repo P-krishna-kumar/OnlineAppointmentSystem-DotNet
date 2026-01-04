47b2730aa6d66419a6821592b292150067954601
P Krishna Kumar <kkrishna37768@gmail.com>
2026-01-04 22:54:22 +0530

    1 chnage

--- Online Appointment System/Models/Appointment.cs
+++ Online Appointment System/Models/Appointment.cs
@@ -1,25 +1,51 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Online_Appointment_System.Models
 {
     public class Appointment
     {
-        public int AppointmentId { get; set; }
+        [Key]
+        public int AppointmentID { get; set; }
 
-        public int UserID { get; set; }
-        public int ServiceID { get; set; }
-        public int SlotID { get; set; }
+        [Required]
+        [Display(Name = "User")]
+        public int UserID { get; set; } // FK to Users
 
+        [Required]
+        [Display(Name = "Service")]
+        public int ServiceID { get; set; } // FK to Services
+
+        [Required]
+        [Display(Name = "Slot")]
+        public int SlotID { get; set; } // FK to TimeSlots
+
+        [Required]
+        [Display(Name = "Date")]
         [DataType(DataType.Date)]
         public DateTime AppointmentDate { get; set; }
 
+        [Required]
         [MaxLength(50)]
         public string Status { get; set; } = "Pending";
 
-        //// Navigation
-        //public User User { get; set; }
-        //public Service Service { get; set; }
-        //public TimeSlot TimeSlot { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Navigation properties
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("ServiceID")]
+        public virtual Service Service { get; set; }
+
+        [ForeignKey("SlotID")]
+        public virtual TimeSlot Slot { get; set; }
+        public DateTime SlotDate { get; internal set; }
+        public TimeSpan StartTime { get; internal set; }
+        public TimeSpan EndTime { get; internal set; }
+        public object TimeSlot { get; internal set; }
     }
 }
