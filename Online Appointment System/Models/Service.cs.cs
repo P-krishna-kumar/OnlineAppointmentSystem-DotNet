bef05d6c854932132956a7c525e3f1f9be64f9c3
P Krishna Kumar <kkrishna37768@gmail.com>
2026-01-07 11:42:07 +0530

    servic model

--- Online Appointment System/Models/Service.cs.cs
+++ Online Appointment System/Models/Service.cs.cs
@@ -1,6 +1,16 @@
-﻿namespace Online_Appointment_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_Appointment_System.Models
 {
     public class Service
     {
+        [Key]
+        public int ServiceID { get; set; }
+        [Required, MaxLength(150)]
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<TimeSlot> TimeSlots { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
