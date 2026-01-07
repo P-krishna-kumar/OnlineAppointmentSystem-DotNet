044d77c6caa05a298edecfd0a66b54a2b6f7f248
P Krishna Kumar <kkrishna37768@gmail.com>
2026-01-07 11:40:35 +0530

    add model account model

---
+++ Online Appointment System/Models/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_Appointment_System.Models
+{
+    public class AccountViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
