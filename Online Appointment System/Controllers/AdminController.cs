d420823f6124f127e38acd458689f3846e5844de
P Krishna Kumar <kkrishna37768@gmail.com>
2026-01-08 17:27:40 +0530

    Appointment for ADO

--- Online Appointment System/Controllers/AdminController.cs
+++ Online Appointment System/Controllers/AdminController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Online_Appointment_System.Data;
+ 
 using Online_Appointment_System.Models;
 using System.Linq;
 using System.Threading.Tasks;
