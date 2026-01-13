5b35ed8ac4744d40d45b9ba8018818ee6b9efa3c
P Krishna Kumar <kkrishna37768@gmail.com>
2026-01-13 15:34:52 +0530

    user dal

--- Online Appointment System/DAL/UserDAL.cs
+++ Online Appointment System/DAL/UserDAL.cs
@@ -79,6 +79,59 @@ namespace Online_Appointment_System.DAL
             return dt;
         }
 
+        public DataTable GetAllUsers()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection con = new SqlConnection(_conn))
+            {
+                SqlCommand cmd = new SqlCommand("sp_User_List", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public void BlockUser(int id)
+        {
+            using (SqlConnection con = new SqlConnection(_conn))
+            {
+                SqlCommand cmd = new SqlCommand("sp_User_Block", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserId", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UnblockUser(int id)
+        {
+            using (SqlConnection con = new SqlConnection(_conn))
+            {
+                SqlCommand cmd = new SqlCommand("sp_User_Unblock", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserId", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            using (SqlConnection con = new SqlConnection(_conn))
+            {
+                SqlCommand cmd = new SqlCommand("sp_User_Delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserId", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
 
     }
 }
